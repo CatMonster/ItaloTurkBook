c54e1508bfe437e3245abda4b963d4a9f4ee4d85
Tommaso Abitante <67195101+JaxzTW@users.noreply.github.com>
2021-12-02 20:58:47 +0100

    Tests

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ main()
 
 async function main() {
   // .env vars
-  const { DEBUG, PAGE_W, PAGE_H, ITALO_LOGIN_USERNAME, ITALO_LOGIN_PASSWORD } = process.env
-
+  const { DEBUG, PAGE_W, PAGE_H } = process.env
+  
   // Init chromium instance
   const browser = await puppeteer.launch({
     headless: Boolean(!DEBUG),
@@ -17,7 +17,21 @@ async function main() {
   })
   const page = await browser.newPage()
   await page.setViewport({ width: 1366, height: 768 })
+  
+  await page.goto('https://www.italotreno.it', { waitUntil: 'networkidle0' })
+  const ato = await page.$$('input[autocomplete=off]')
+  //from.type("Roma")
+  //to.type("Steve")
 
+  console.log(ato.lenght)
+
+  //Individuare se il posto è occupato o non
+  //document.querySelectorAll(".aUnit.unitGroup0")[numero_elemento_array].style.backgroundImage
+  
+  //Individuare il numero del posto (Numero elemento array != numero posto)
+  //document.querySelectorAll(".aUnit.unitGroup0")[numero_elemento_array].querySelector("span").innerText
+  
+/*{
   // -------------------------
   // NSFW things start here 😏
   // -------------------------
@@ -43,4 +57,5 @@ async function main() {
 
   // Click on "Prenota" button
   await page.click('._inl-ctaButton._inl-ctaBlock')
+}*/
 }
