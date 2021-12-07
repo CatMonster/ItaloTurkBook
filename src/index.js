94d6b0d194d915535966713f05a05a04ac1434a9
Tommaso Abitante <67195101+JaxzTW@users.noreply.github.com>
2021-12-07 18:51:46 +0100

    Working select destination and continue button

--- src/index.js
+++ src/index.js
@@ -14,16 +14,39 @@ async function main() {
   const browser = await puppeteer.launch({
     headless: Boolean(!DEBUG),
     args: [`--window-size=${PAGE_W},${PAGE_H}`],
+    slowMo: 80
   })
   const page = await browser.newPage()
   await page.setViewport({ width: 1366, height: 768 })
   
-  await page.goto('https://www.italotreno.it', { waitUntil: 'networkidle0' })
-  const ato = await page.$$('input[autocomplete=off]')
-  //from.type("Roma")
-  //to.type("Steve")
+  await page.goto('https://biglietti.italotreno.it/Booking_Acquisto_SelezioneTreno_A.aspx', { waitUntil: 'networkidle0' })
+  const oplist = await page.$$('.ghots-selection')
 
-  console.log(ato.lenght)
+  //Activate and open the first list
+  await oplist[0].click()
+  await oplist[0].click()
+
+  //Click on the departure station
+  const listd = await page.$$('.destination__box--direct .list span')
+  await listd[0].click()
+
+  //The same thing as before for the other part
+  await page.$$('.ghots-selection')
+  await oplist[3].click()
+  const lista = await page.$$('.destination__box--direct .list span')
+  await lista[0].click()
+
+  await page.click('#ModuloRicercaBookingRicercaRestylingBookingSelezioneTrenoAView_ButtonChangeSearchInBookingFlow',{waitUntil: 'networkidle2'})
+
+  /*const go = await page.$('#ModuloRicercaBookingRicercaRestylingBookingSelezioneTrenoAView_ButtonChangeSearchInBookingFlow')
+  await go.click()*/
+
+  //Select the first train
+  // const train = await page.$("#booking-flow > div.booking-flow-elenco-treni > div.main-section.no-padding-vert > div.lista-treni > div:nth-child(2) > div.accordion-header")
+  // await train.click()
+  
+  //Selezione costo biglietto/accesso alla selezione posti
+  //document.querySelectorAll('.button-tariffa label')[0].click()
 
   //Individuare se il posto è occupato o non
   //document.querySelectorAll(".aUnit.unitGroup0")[numero_elemento_array].style.backgroundImage
@@ -31,31 +54,7 @@ async function main() {
   //Individuare il numero del posto (Numero elemento array != numero posto)
   //document.querySelectorAll(".aUnit.unitGroup0")[numero_elemento_array].querySelector("span").innerText
   
-/*{
-  // -------------------------
-  // NSFW things start here 😏
-  // -------------------------
-
-  // Open italo login page and wait until there are no more than 0 network connections for at least 500 ms.
-  await page.goto('https://biglietti.italotreno.it/Booking_Acquisto_Ricerca.aspx', { waitUntil: 'networkidle0' })
-
-  // Open login popup
-  await page.click( '.italo_button_red.fancybox')
-  
   // Fill out login credentials and clicks login button
-  await page.type('#MasterHeaderRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuMemberLoginRestylingBookingAcquistoRicercaView_TextBoxUserID', ITALO_LOGIN_USERNAME)
-  await page.type('#MasterHeaderRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuMemberLoginRestylingBookingAcquistoRicercaView_PasswordFieldPassword', ITALO_LOGIN_PASSWORD)
-  await page.$eval('#MasterHeaderRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuMemberLoginRestylingBookingAcquistoRicercaView_ButtonLogIn', form => form.click())
-  
-  // Wait until user is logged by html checking user menu
-  await page.waitForSelector('.adacto.ada-user-button.ada-user-button-social', { visible: true, timeout: 0 })
-  
-  // Open carnet page
-  await page.goto('https://biglietti.italotreno.it/Customer_Account_MieiAcquisti_MieiCarnet.aspx', { waitUntil: 'networkidle0' })
-
-  // TODO - Verify pagination order
-
-  // Click on "Prenota" button
-  await page.click('._inl-ctaButton._inl-ctaBlock')
-}*/
+  // await page.type('#MasterHeaderRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuMemberLoginRestylingBookingAcquistoRicercaView_TextBoxUserID', ITALO_LOGIN_USERNAME)
+  // await page.type('#MasterHeaderRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuRestylingBookingAcquistoRicercaView_MasterHeaderGlobalMenuMemberLoginRestylingBookingAcquistoRicercaView_PasswordFieldPassword', ITALO_LOGIN_PASSWORD)
 }
